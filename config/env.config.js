87bf5649aa4bdafe4345f20707703818547b3be5
Oleksandr Kuzmenko <oleksandr_kuzmenko1@epam.com>
2018-07-10 19:36:19 +0300

    mongoDb server configuration

---
+++ config/env.config.js
@@ -0,0 +1,7 @@
+const config = {
+  mongoPort: 27017,
+  mongoDbPath: 'C:\\Users\\Oleksandr_Kuzmenko1\\Desktop\\lern\\mongoDbRepository',
+  mongoExePath: 'C:\\Program Files\\MongoDB\\Server\\4.0\\bin\\mongod',
+}
+
+module.exports = config
