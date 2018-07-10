08642ea0f434978b0656703d2de6ab9125f9f912
Oleksandr Kuzmenko <oleksandr_kuzmenko1@epam.com>
2018-07-10 19:37:41 +0300

    code review

--- bin/mongoLocalServer.js
+++ bin/mongoLocalServer.js
@@ -1,17 +1,17 @@
-const Mongod = require('mongod')
-const { mongoPort, mongoExePath, mongoDbPath } = require('../config/env.config')
+const Mongod = require('mongod');
+const { mongoPort, mongoExePath, mongoDbPath } = require('../config/env.config');
 
 const server = new Mongod({
   port: mongoPort,
   bin: mongoExePath,
   dbpath: mongoDbPath,
-})
+});
 
 server
   .open()
   .then(() => {
-    console.log('MongoDb Server is running')
+    console.log('MongoDb Server is running');
   })
   .catch(err => {
-    console.log(err)
-  })
+    console.log(err);
+  });
--- config/env.config.js
+++ config/env.config.js
@@ -2,6 +2,6 @@ const config = {
   mongoPort: 27017,
   mongoDbPath: 'C:\\Users\\Oleksandr_Kuzmenko1\\Desktop\\lern\\mongoDbRepository',
   mongoExePath: 'C:\\Program Files\\MongoDB\\Server\\4.0\\bin\\mongod',
-}
+};
 
-module.exports = config
+module.exports = config;
